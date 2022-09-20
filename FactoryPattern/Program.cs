ba4e925ed847e78976f64fc855abfdae362ff60f
Xavier Barnes <xdbcoder44@gmail.com>
2022-09-20 19:20:02 -0400

    Add project files.

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryPattern
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What type of vehicle do you want to create");
+
+            string vehicle = Console.ReadLine();    
+
+            IVehicle phone = VehicleFactory.GetVehicle(vehicle);
+            phone.Drive();
+            Console.ReadLine();
+
+        }
+    }
+}
